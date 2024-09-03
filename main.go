e53e2b084ecf8964b28f5f15cc3ddf3e38fd49a4
良药 <liangyao0506@gmail.com>
2024-09-03 21:36:40 +0800

    v1

---
+++ main.go
@@ -0,0 +1,201 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for {
+		mainFatRateBody()
+		if cont := whetherContinue(); !cont {
+			break
+		}
+	}
+
+}
+
+func mainFatRateBody() {
+	//录入各项
+	weight, tall, age, sexWeight, sex := getMaterialsFromInput()
+
+	//计算体脂率
+	fatRate := calcFatRate(weight, tall, age, sexWeight)
+	if fatRate<=0{
+		panic("fat rate is not allowed to be negative")
+	}
+	//给出建议
+	getHealthinessSuggestions(sex, age, fatRate)
+}
+
+func getHealthinessSuggestions(sex string, age int, fatRate float64) {
+	//给出建议
+	if sex == "男" {
+		//  编写男性的体脂率与体脂状态表
+		if age >= 18 && age <= 39 {
+			// 对该年龄段的体脂率进行if、else判断
+			if fatRate <= 0.1 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：偏瘦")
+			} else if fatRate > 0.1 && fatRate <= 0.16 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：标准")
+			} else if fatRate > 0.16 && fatRate <= 0.21 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：偏胖")
+			} else if fatRate > 0.21 && fatRate <= 0.26 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：肥胖")
+			} else {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：非常肥胖")
+			}
+		} else if age >= 40 && age <= 59 {
+			// 对该年龄段的体脂率进行if、else判断
+			if fatRate <= 0.11 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：偏瘦")
+			} else if fatRate > 0.11 && fatRate <= 0.17 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：标准")
+			} else if fatRate > 0.17 && fatRate <= 0.22 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：偏胖")
+			} else if fatRate > 0.22 && fatRate <= 0.27 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：肥胖")
+			} else {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：非常肥胖")
+			}
+		} else if age >= 60 {
+			// 对该年龄段的体脂率进行if、else判断
+			if fatRate <= 0.13 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：偏瘦")
+			} else if fatRate > 0.13 && fatRate <= 0.19 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：标准")
+			} else if fatRate > 0.19 && fatRate <= 0.24 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：偏胖")
+			} else if fatRate > 0.24 && fatRate <= 0.29 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：肥胖")
+			} else {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：非常肥胖")
+			}
+		} else {
+			fmt.Println("我们不看未成年人的体脂率，变化太大，无法评判")
+		}
+	} else {
+		// 编写女性的体脂率与体脂状态表
+		if age >= 18 && age <= 39 {
+			// 对该年龄段的体脂率进行if、else判断
+			if fatRate <= 0.2 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：偏瘦")
+			} else if fatRate > 0.2 && fatRate <= 0.27 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：标准")
+			} else if fatRate > 0.27 && fatRate <= 0.34 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：偏胖")
+			} else if fatRate > 0.34 && fatRate <= 0.39 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：肥胖")
+			} else {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：非常肥胖")
+			}
+		} else if age >= 40 && age <= 59 {
+			// 对该年龄段的体脂率进行if、else判断
+			if fatRate <= 0.21 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：偏瘦")
+			} else if fatRate > 0.21 && fatRate <= 0.28 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：标准")
+			} else if fatRate > 0.28 && fatRate <= 0.35 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：偏胖")
+			} else if fatRate > 0.35 && fatRate <= 0.40 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：肥胖")
+			} else {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：非常肥胖")
+			}
+		} else if age >= 60 {
+			// 对该年龄段的体脂率进行if、else判断
+			if fatRate <= 0.22 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：偏瘦")
+			} else if fatRate > 0.22 && fatRate <= 0.29 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：标准")
+			} else if fatRate > 0.29 && fatRate <= 0.36 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：偏胖")
+			} else if fatRate > 0.36 && fatRate <= 0.41 {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：肥胖")
+			} else {
+				// 得出结论，并进行建议
+				fmt.Println("目前是：非常肥胖")
+			}
+		} else {
+			fmt.Println("我们不看未成年人的体脂率，变化太大，无法评判")
+		}
+	}
+}
+
+func calcFatRate(weight float64, tall float64, age int, sexWeight int) float64 {
+	//计算体脂率
+	bmi := weight / (tall * tall)
+	fatRate := (1.2*bmi + 0.23*float64(age) - 5.4 - 10.8*float64(sexWeight)) / 100
+	fmt.Println("体脂率是", fatRate)
+	return fatRate
+}
+
+func getMaterialsFromInput() (float64, float64, int, int, string) {
+	//录入各项
+	var name string
+	fmt.Print("姓名: ")
+	fmt.Scanln(&name)
+
+	var weight float64
+	fmt.Print("体重（千克）：") //辅助信息
+	fmt.Scanln(&weight)  //录入命令
+
+	var tall float64
+	fmt.Print("身高（米）：")
+	fmt.Scanln(&tall)
+
+	var age int
+	fmt.Print("年龄：")
+	fmt.Scanln(&age)
+
+	var sexWeight int
+	sex := "男"
+	fmt.Print("性别（男/女）：")
+	fmt.Scanln(&sex)
+	if sex == "男" {
+		sexWeight = 1
+	} else {
+		sexWeight = 0
+	}
+	return weight, tall, age, sexWeight, sex
+}
+
+func whetherContinue() bool {
+	var whetherContinue string
+	fmt.Print("是否录入下一个（y/n）?")
+	fmt.Scanln(&whetherContinue)
+	if whetherContinue != "y" {
+		return false
+	}
+	return true
+}
+
+
